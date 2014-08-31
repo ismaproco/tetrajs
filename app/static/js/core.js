db38dac6e5c2123822ff8b9255682f1594a5b768
ismael <ismael.jimenez@gmail.com>
2014-08-31 15:39:16 -0600

    Added core file as static file to manage the tetrajs game, added jquery as external library

---
+++ app/static/js/core.js
@@ -0,0 +1,14 @@
+// ----------------------------------------------------------------------------
+//     tetra.js 0.0.1
+//     Author: Ismael Jimenez
+//     Description: Core managment of the tetris app    
+//     08/2014 
+// ----------------------------------------------------------------------------
+
+
+// Create a matrix of 10 columns x 20 rows
+// Create the tetris objects
+// Create the method to generate figures
+// Create the method to move figures in the matrix
+// Create the method to to remove lines
+// Create the method to keep the score

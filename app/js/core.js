bcd7b7325be61e0e61434fa3a087b0e64f895551
Ismael Jimenez <ijimenezgarzon@gmail.com>
2014-09-08 09:53:49 -0600

    Add collision detection algorithm, and modified the addFigure function to add temporal figures

--- app/js/core.js
+++ app/js/core.js
@@ -19,286 +19,11 @@ function resetMatrix()
     }    
 }
 
-
-// Create the tetris figures
-var figures = {};
-
-
-//Define each one of the terms and their rotations
-function OTerm()
-{
-    this.size = { x:2 , y:2 };
-    this.rotations = 1;
-    this.def = [[]];
-    
-    this.def[0][ 0 +"_"+ 0 ] = 1;
-    this.def[0][ 1 +"_"+ 0 ] = 1;
-    this.def[0][ 0 +"_"+ 1 ] = 1;
-    this.def[0][ 1 +"_"+ 1 ] = 1;
-}
-
-figures.oter = new OTerm();
-
-// The T figure is a 3x3 grid
-// and they rotate from the center of the grid
-//           _ _ _ _      _ _ _
-//        1 |_|x|_|_|  2 |x|x|x|x|
-//          |_|x|_|_|    |_| |_|_|
-//          |_|x|_|_|    |_| |_|_|
-//          |_|x|_|_|    |_| |_|_|
-// 
-
-
-function ITerm()
-{
-    this.size = { x:4 , y:4 };
-    this.rotations = 2;
-
-    this.def = [ [] , [] ];
-
-    this.def[0][ 1 +"_"+ 0 ] = 1;
-    this.def[0][ 1 +"_"+ 1 ] = 1;
-    this.def[0][ 1 +"_"+ 2 ] = 1;
-    this.def[0][ 1 +"_"+ 3 ] = 1;
-
-    this.def[1][ 0 +"_"+ 3 ] = 1;
-    this.def[1][ 1 +"_"+ 3 ] = 1;
-    this.def[1][ 2 +"_"+ 3 ] = 1;
-    this.def[1][ 3 +"_"+ 3 ] = 1;
-
-}
-
-figures.iter = new ITerm();
-
-// The T figure is a 3x3 grid
-// and they rotate from the center of the grid
-//           _ _ _      _ _ _
-//        1 |_|_|_|  2 |x|_|_|
-//          |_|x|_|    |x|x|_|
-//          |x|x|x|    |x|_|_|
-//           ¯ ¯ ¯
-//           _ _ _      _ _ _
-//        4 | |x|_|  3 |x|x|x|
-//          |x|x|_|    | |x| |
-//          | |x| |    |_|_|_|
-//           ¯ ¯ ¯
-
-function TTerm()
-{
-    this.size = { x:3 , y:3 };
-    this.rotations = 4;
-
-    this.def = [ [] , [] , [] , [] ];
-
-    // rotation 0
-    this.def[0][ 0 +"_"+ 0 ] = 1;
-    this.def[0][ 1 +"_"+ 0 ] = 1;
-    this.def[0][ 2 +"_"+ 0 ] = 1;
-        
-    this.def[0][ 1 +"_"+ 1 ] = 1;
-    
-    // rotation 1
-    this.def[1][ 1 +"_"+ 1 ] = 1;
-
-    this.def[1][ 0 +"_"+ 0 ] = 1;
-    this.def[1][ 0 +"_"+ 1 ] = 1;
-    this.def[1][ 0 +"_"+ 2 ] = 1;
-
-    // rotation 2
-    
-    this.def[2][ 1 +"_"+ 1 ] = 1;
-    
-    this.def[2][ 0 +"_"+ 2 ] = 1;
-    this.def[2][ 1 +"_"+ 2 ] = 1;
-    this.def[2][ 2 +"_"+ 2 ] = 1;
-
-    // rotation 3
-    
-    this.def[3][ 1 +"_"+ 1 ] = 1;
-    
-    this.def[3][ 2 +"_"+ 0 ] = 1;
-    this.def[3][ 2 +"_"+ 1 ] = 1;
-    this.def[3][ 2 +"_"+ 2 ] = 1;
-
-}
-
-figures.tter = new TTerm();
-
-// The L figure is a 3x3 grid
-// and they rotate from the center of the grid
-//           _ _ _      _ _ _
-//        1 |_|x|_|  2 |x|x|x|
-//          |_|x|_|    |x|_|_|
-//          | |x|x|    |_|_|_|
-//           ¯ ¯ ¯
-//           _ _ _      _ _ _
-//        4 |_|_|_|  3 |x|x|_|
-//          |_|_|x|    |_|x|_|
-//          |x|x|x|    |_|x|_|
-//           ¯ ¯ ¯
-
-function LTerm()
-{
-    this.size = { x:3 , y:3 };
-    this.rotations = 4;
-
-    this.def = [ [] , [] , [] , [] ];
-
-    // rotation 0
-    this.def[0][ 1 +"_"+ 0 ] = 1;
-    this.def[0][ 1 +"_"+ 1 ] = 1;
-    this.def[0][ 1 +"_"+ 2 ] = 1;
-
-    this.def[0][ 2 +"_"+ 0 ] = 1;
-    
-    // rotation 1
-    this.def[1][ 0 +"_"+ 1 ] = 1;
-
-    this.def[1][ 0 +"_"+ 2 ] = 1;
-    this.def[1][ 1 +"_"+ 2 ] = 1;
-    this.def[1][ 2 +"_"+ 2 ] = 1;
-
-    // rotation 2
-    
-    this.def[2][ 1 +"_"+ 0 ] = 1;
-    this.def[2][ 1 +"_"+ 1 ] = 1;
-    this.def[2][ 1 +"_"+ 2 ] = 1;
-
-    this.def[2][ 0 +"_"+ 2 ] = 1;
-
-    // rotation 3
-    
-    this.def[3][ 0 +"_"+ 0 ] = 1;
-    this.def[3][ 1 +"_"+ 0 ] = 1;
-    this.def[3][ 2 +"_"+ 0 ] = 1;
-
-    this.def[3][ 2 +"_"+ 1 ] = 1;
-}
-
-figures.lter = new LTerm();
-
-
-// The J figure is a 3x3 grid
-// and they rotate from the center of the grid
-//           _ _ _      _ _ _
-//        1 |_|x|_|  2 | | | |
-//          |_|x|_|    |x|_|_|
-//          |x|x| |    |x|x|x|
-//           ¯ ¯ ¯      ¯ ¯ ¯
-//           _ _ _      _ _ _
-//        4 |_|_|_|  3 |_|x|x|
-//          |x|x|x|    |_|x|_|
-//          | | |x|    |_|x|_|
-//           ¯ ¯ ¯
-
-function JTerm()
-{
-    this.size = { x:3 , y:3 };
-    this.rotations = 4;
-
-    this.def = [ [] , [] , [] , [] ];
-
-    // rotation 0
-    this.def[0][ 0 +"_"+ 0 ] = 1;
-    this.def[0][ 1 +"_"+ 0 ] = 1;
-    this.def[0][ 1 +"_"+ 1 ] = 1;
-
-    this.def[0][ 1 +"_"+ 2 ] = 1;
-    
-    // rotation 1
-    this.def[1][ 0 +"_"+ 1 ] = 1;
-
-    this.def[1][ 0 +"_"+ 0 ] = 1;
-    this.def[1][ 1 +"_"+ 0 ] = 1;
-    this.def[1][ 2 +"_"+ 0 ] = 1;
-
-    // rotation 2
-    
-    this.def[2][ 1 +"_"+ 0 ] = 1;
-    this.def[2][ 1 +"_"+ 1 ] = 1;
-    this.def[2][ 1 +"_"+ 2 ] = 1;
-
-    this.def[2][ 2 +"_"+ 2 ] = 1;
-
-    // rotation 3
-    
-    this.def[3][ 0 +"_"+ 1 ] = 1;
-    this.def[3][ 1 +"_"+ 1 ] = 1;
-    this.def[3][ 2 +"_"+ 1 ] = 1;
-
-    this.def[3][ 2 +"_"+ 0 ] = 1;
-}
-
-figures.jter = new JTerm();
-
-
-// The S figure is a 3x3 grid
-// and they rotate from the center of the grid
-//           _ _ _      _ _ _
-//        1 |_|_|_|  2 |x| | |
-//          |_|x|x|    |x|x| |
-//          |x|x| |    | |x| |
-//           ¯ ¯ ¯      ¯ ¯ ¯
-
-function STerm()
-{
-    this.size = { x:3 , y:3 };
-    this.rotations = 2;
-
-    this.def = [ [] , [] ];
-
-    // rotation 0
-    this.def[0][ 0 +"_"+ 0 ] = 1;
-    this.def[0][ 1 +"_"+ 0 ] = 1;
-    this.def[0][ 1 +"_"+ 1 ] = 1;
-    this.def[0][ 2 +"_"+ 1 ] = 1;
-    
-    // rotation 1
-    this.def[1][ 1 +"_"+ 0 ] = 1;
-    this.def[1][ 0 +"_"+ 1 ] = 1;
-    this.def[1][ 1 +"_"+ 1 ] = 1;
-    this.def[1][ 0 +"_"+ 2 ] = 1;
-}
-
-figures.ster = new STerm();
-
-
-// The Z figure is a 3x3 grid
-// and they rotate from the center of the grid
-//           _ _ _      _ _ _
-//        1 |_|_|_|  2 | | |x|
-//          |x|x| |    |_|x|x|
-//          | |x|x|    | |x| |
-//           ¯ ¯ ¯      ¯ ¯ ¯
-
-function ZTerm()
-{
-    this.size = { x:3 , y:3 };
-    this.rotations = 2;
-
-    this.def = [ [] , [] ];
-
-    // rotation 0
-    this.def[0][ 1 +"_"+ 0 ] = 1;
-    this.def[0][ 2 +"_"+ 0 ] = 1;
-    this.def[0][ 0 +"_"+ 1 ] = 1;
-    this.def[0][ 1 +"_"+ 1 ] = 1;
-    
-    // rotation 1
-    this.def[1][ 1 +"_"+ 0 ] = 1;
-    this.def[1][ 1 +"_"+ 1 ] = 1;
-    this.def[1][ 2 +"_"+ 1 ] = 1;
-    this.def[1][ 2 +"_"+ 2 ] = 1;
-}
-
-figures.zter = new ZTerm();
-
-
 // Create the method to generate figures in matrix
 // figureName: [oter, iter, tter, ...]
 // position: {x:0,y:0};
 
-function addFigure(figureName, position, rot ) 
+function addFigure(figureName, position, rot, temp ) 
 {
 
     var rotation = rot || 0;
@@ -330,11 +55,19 @@ function addFigure(figureName, position, rot )
     {
         for(var j = 0; j < size.y; j+= 1) 
         {
-            m[(position.x + i)+'_'+(position.y + j) ] = 
-                 figures[figureName].def[rotation][ i + "_" + j];
+            if(figures[figureName].def[rotation][ i + "_" + j] === 1)
+            {
+                if(!temp)
+                {
+                    m[(position.x + i)+'_'+(position.y + j) ] = 1;
+                }
+                else
+                {
+                    m[(position.x + i)+'_'+(position.y + j) ] = -1;
+                }
+            }
         }
     }
-
 }
 
 
@@ -365,12 +98,17 @@ function drawMatrixInHTML()
     
     for (var j = 0; j < 20; j++) {
         for (var i = 0; i < 10; i++) {
+            var cellId = i + '_' + j;
+            
             if( m[i+'_'+j] === 1 )
             {
-                var cellId = i + '_' + j;
                 $('#m_' + cellId).addClass('red');
                 //drawCache.push(cellId);
                 //m[ cellId ] = 0;
+            }
+            else if(m[i+'_'+j] === -1)
+            {
+                $('#m_' + cellId).addClass('green');
             }
         }
     }    
@@ -400,21 +138,78 @@ function mainLoop()
                         ];
 
     var flagAddFigure = true;
+    var position = {x:4,y:19};
+    var rIndex = getRandomInt(0,figuresList.length);
 
     setInterval(function(){
 
+        
+        var result = figureCanBeAdded( m, figuresList[rIndex] , position , 0 );
+
+        if( result )
+        {
+            addFigure(figuresList[rIndex], position , 0 ,true);
+        }
+        else
+        {
+            addFigure(figuresList[rIndex], position , 0 ,false);
+        }
+
         if( flagAddFigure )
         {
-            //generate the figure Random Index
-            var rIndex = getRandomInt(0,figuresList.length);
-            console.log(rIndex);
-            addFigure(figuresList[rIndex], {x:4,y:19} , 0 );    
-
-            flagAddFigure = false;
+            
         }
+
+        console.log( result,position.y );
         
     },1000);
 }
+
+// Evaluate if a figure can be set in the specific position
+function figureCanBeAdded( matrix , figureName , position , rot )
+{
+    var rotation = rot || 0;
+    var posY = position.y;
+    var posX = position.x;
+
+
+    var size = {};
+    
+    if( rotation >= figures[figureName].rotations )
+    {
+        rotation = rotation % figures[figureName].rotations;
+    }
+
+
+    if( rotation % 2 === 0 )
+    {
+        size = figures[figureName].size;    
+    }else
+    {
+        size = {
+            x: figures[figureName].size.y,
+            y: figures[figureName].size.x
+        };
+    }
+    
+
+    // change the y position to start the draw in cartesian coordinates
+    posY -= size.y - 1;
+
+    for(var i = 0; i < size.x; i+= 1)
+    {
+        for(var j = 0; j < size.y; j+= 1) 
+        {
+            if( matrix[ (position.x + i) + '_' + (posY + j) ] === 1 )
+            {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
 
 
 // Create the method to to remove lines

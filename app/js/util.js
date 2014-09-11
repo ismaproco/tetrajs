a86646a07f5d4caceea485df74844cdca2fae9e9
Ismael Jimenez <ijimenezgarzon@gmail.com>
2014-09-11 11:25:53 -0600

    Removed old files

--- app/js/util.js
+++
@@ -1,6 +0,0 @@
-// from
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-// Returns a random number between min (inclusive) and max (exclusive)
-function getRandomInt(min, max) {
-  return parseInt(Math.random() * (max - min) + min , 0);
-}

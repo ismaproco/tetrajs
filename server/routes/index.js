bb0f8d5a56477e6da9274b869e001b564c1cafc2
ismael <ismael.jimenez@gmail.com>
2014-09-14 16:22:09 -0600

    Added routes to manage the API calls

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,4 +6,35 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+
+/* GET Start Game */
+router.get('/startGame', function(req, res) {
+  
+});
+
+/* GET getBoards */
+
+router.get('/getBoards', function(req, res) {
+  
+});
+
+/* GET generateFigure */
+
+router.get('/generateFigure', function(req, res) {
+  
+});
+
+/* GET moveFigure */
+
+router.get('/moveFigure', function(req, res) {
+  
+});
+
+/* GET getToken */
+
+router.get('/getToken', function(req, res) {
+  
+});
+
+
 module.exports = router;

5684a64dfc13875a6371590caa5dd6d7579fb9b0
ismael <ismael.jimenez@gmail.com>
2014-09-16 22:37:06 -0600

    Added spec for the insertion of documents

--- server/spec/mongoHelperSpec.js
+++ server/spec/mongoHelperSpec.js
@@ -23,3 +23,30 @@ describe('Open connection',function(){
     });
 
 });
+
+
+describe('Insert Document',function(){
+	var result = {};
+	
+	var board = {
+			token:'main_board_1',
+			description: 'this is a board'
+		};
+
+	beforeEach(function(done){
+		helper.insertDocument('boards',board,function(err, internalResult){
+			result = internalResult;
+			done();
+		});
+	});
+
+	it('The document is going to be inserted', function() {
+		expect(result.err).toEqual(null);
+	})
+
+	// Code to Execute after the async spec
+    afterEach(function(done) {
+      done();
+    });
+
+});

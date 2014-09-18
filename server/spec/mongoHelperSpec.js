921815b395804a1d9a3a2ac3d79b2ec1a0ae22b3
Ismael Jimenez <ijimenezgarzon@gmail.com>
2014-09-17 23:53:59 -0600

    fixed mongoHelper Spec

--- server/spec/mongoHelperSpec.js
+++ server/spec/mongoHelperSpec.js
@@ -42,7 +42,8 @@ describe('Insert Document',function(){
 	});
 
 	it('The document is going to be inserted', function() {
-		expect(result._id.length).toBeGreaterThan(0);
+		expect(result.err).toEqual(null);
+		expect(result.length).toBeGreaterThan(0);
 	})
 
 	// Code to Execute after the async spec
@@ -51,36 +52,6 @@ describe('Insert Document',function(){
     });
 
 });
-
-
-describe('Insert Document',function(){
-	var result = {};
-	
-	var board = {
-			token:'main_board_1',
-			description: 'this is a board'
-		};
-
-	beforeEach(function(done){
-		helper.insertDocument('boards',board,function(err, internalResult){
-			result = internalResult;
-			result.err = err;
-			done();
-		});
-	});
-
-	it('One document is inserted', function() {
-		expect(result._id.length).toBeGreaterThan(0);
-	})
-
-	// Code to Execute after the async spec
-    afterEach(function(done) {
-      done();
-    });
-
-});
-
-
 
 describe('get Documents no filter',function(){
 	var result = {};
@@ -93,7 +64,8 @@ describe('get Documents no filter',function(){
 		});
 	});
 
-	it('Get more than one document from the database', function() {
+	it('Get more than zero documents from the database', function() {
+		expect(result.err).toEqual(null);
 		expect(result.length).toBeGreaterThan(0);
 	})
 

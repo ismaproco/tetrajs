bae4e2726602e2d8525770093ae8e1ff0135c97b
Ismael Jimenez <ijimenezgarzon@gmail.com>
2014-09-19 23:04:04 -0600

    Created util file with the useful methods for the development

---
+++ server/bin/util.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var util = {};
+
+/*
+	Generates a random string with a length of the specified parameter
+*/
+
+util.generateRandomString = function randomString( charLength )
+{
+    var text = "";
+    var possible = 	'ABCDEFGHIJKLMNOPQRSTUVWXYZabcde'+
+    				'fghijklmnopqrstuvwxyz0123456789';
+
+    for( var i=0; i < charLength; i++ )
+    {
+        text += possible.charAt( 
+        			Math.floor( Math.random() * possible.length ) );
+    }
+
+    return text;
+}
+
+modules.export = util;
